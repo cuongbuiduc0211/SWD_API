dbae6ed4fa1acb558fdfb96f392c1bead444e445
TsunMoon <kimhexi1998@gmail.com>
2020-11-11 23:42:26 +0700

    edit list request

--- ContentOutSourceAPI/Controllers/UsersHavingPostsController.cs
+++ ContentOutSourceAPI/Controllers/UsersHavingPostsController.cs
@@ -64,22 +64,90 @@ namespace ContentOutSourceAPI.Controllers
         }
 
         [HttpPost("requestedPosts")]
-        public async Task<ActionResult<List<TblPosts>>> GetRequestedPosts(UsernameDTO usernameDTO)
+        public async Task<ActionResult<List<PostAndKeyword>>> GetRequestedPosts(UsernameDTO usernameDTO)
         {
             List<TblPosts> list = _context.TblPosts
                 .FromSqlRaw("select * from tblPosts where Id in " +
                 "(select PostId from tblUsersHavingPosts where Username = {0} and Status = 'requested')", usernameDTO.Username)
                 .ToList<TblPosts>();
+
+            List<TblPosts> listResponse = new List<TblPosts>();
+            List<PostAndKeyword> listPostAndKeywordResponse = new List<PostAndKeyword>();
             if (list.Count > 0)
             {
-                return list;
+               for(int i = 0; i < list.Count; i++)
+                {
+                    TblPosts currentPost = list[i];
+                    if (currentPost.PostType.Equals("Writer"))
+                    {
+                        listResponse.Add(currentPost);
+
+                        PostAndKeyword postAndKeyword = new PostAndKeyword();
+                        postAndKeyword.Id = currentPost.Id;
+                        postAndKeyword.Title = currentPost.Title;
+                        postAndKeyword.Description = currentPost.Description;
+                        postAndKeyword.CharacterLimit = currentPost.CharacterLimit;
+                        postAndKeyword.Amount = currentPost.Amount;
+                        postAndKeyword.PostType = currentPost.PostType;
+                        postAndKeyword.RelatedDocument = currentPost.RelatedDocument;
+                        postAndKeyword.IsPublic = currentPost.IsPublic;
+                        postAndKeyword.CreatedDate = currentPost.CreatedDate;
+                        postAndKeyword.Status = currentPost.Status;
+                        postAndKeyword.listKeywords = findListKeyByPostId(currentPost.Id);
+
+                        listPostAndKeywordResponse.Add(postAndKeyword);
+
+                    }
+                }
+                return listPostAndKeywordResponse;
             }
             return null;
         }
 
+        //Nhận vào PostID và trả về 1 list các bài Keyword theo PostID
+        private List<string> findListKeyByPostId(int id)
+        {
+            List<TblPostsHavingKeywords> listHavingKeyword = _context.TblPostsHavingKeywords.ToList<TblPostsHavingKeywords>();
+            List<TblKeywords> listTblKeywords = _context.TblKeywords.ToList<TblKeywords>();
+
+            List<int?> listKeywordID = new List<int?>();
+            List<string> listKeywordResponse = new List<string>();
+
+            // Lấy ra 1 list KeywordID
+            for (int i = 0; i < listHavingKeyword.Count; i++)
+            {
+                TblPostsHavingKeywords current = listHavingKeyword[i];
+
+                if (id == current.PostId)
+                {
+                    listKeywordID.Add(current.KeywordId);
+                }
+            }
+
+            // Chạy vòng for để tìm ra List Keyword
+            if (listKeywordID != null)
+            {
+                for (int i = 0; i < listKeywordID.Count; i++)
+                {
+                    int? currentListID = listKeywordID[i];
+
+                    for (int j = 0; j < listTblKeywords.Count; j++)
+                    {
+                        TblKeywords currentTblKeyword = listTblKeywords[j];
+                        if (currentTblKeyword.Id == currentListID)
+                        {
+                            listKeywordResponse.Add(currentTblKeyword.Name);
+                        }
+                    }
+                }
+            }
+
+            return listKeywordResponse;
+        }
+
 
         [HttpPost("acceptedPosts")]
-        public async Task<ActionResult<List<TblPosts>>> GetAcceptedPosts(UsernameDTO usernameDTO)
+        public async Task<ActionResult<List<PostAndKeyword>>> GetAcceptedPosts(UsernameDTO usernameDTO)
         {
 
             List<TblUsersHavingPosts> listRequested = _context.TblUsersHavingPosts
@@ -89,11 +157,39 @@ namespace ContentOutSourceAPI.Controllers
                 .FromSqlRaw("select * from tblPosts where Id in " +
                 "(select PostId from tblUsersHavingPosts where Username = {0} and Status = 'accepted')", usernameDTO.Username)
                 .ToList<TblPosts>();
+
+            List<TblPosts> listResponse = new List<TblPosts>();
+            List<PostAndKeyword> listPostAndKeywordResponse = new List<PostAndKeyword>();
             if (listAccepted.Count > 0)
             {
+                
+                for (int i = 0; i < listAccepted.Count; i++)
+                {
+                    TblPosts currentPost = listAccepted[i];
+                    if (currentPost.PostType.Equals("Writer"))
+                    {
+                        listResponse.Add(currentPost);
+
+                        PostAndKeyword postAndKeyword = new PostAndKeyword();
+                        postAndKeyword.Id = currentPost.Id;
+                        postAndKeyword.Title = currentPost.Title;
+                        postAndKeyword.Description = currentPost.Description;
+                        postAndKeyword.CharacterLimit = currentPost.CharacterLimit;
+                        postAndKeyword.Amount = currentPost.Amount;
+                        postAndKeyword.PostType = currentPost.PostType;
+                        postAndKeyword.RelatedDocument = currentPost.RelatedDocument;
+                        postAndKeyword.IsPublic = currentPost.IsPublic;
+                        postAndKeyword.CreatedDate = currentPost.CreatedDate;
+                        postAndKeyword.Status = currentPost.Status;
+                        postAndKeyword.listKeywords = findListKeyByPostId(currentPost.Id);
+
+                        listPostAndKeywordResponse.Add(postAndKeyword);
+
+                    }
+                }
                 _context.TblUsersHavingPosts.RemoveRange(listRequested);
                 await _context.SaveChangesAsync();
-                return listAccepted;
+                return listPostAndKeywordResponse;
 
             }
             return null;
